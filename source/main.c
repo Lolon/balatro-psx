93ef4768f07ecb8e744a7b90431c9e4883de2114
Lolon <lolondev@mail.com>
2025-09-16 09:48:04 +0100

    hello world achieved

--- source/main.c
+++ source/main.c
@@ -1,4 +1,4 @@
-#include <tonc.h>
+/* #include <tonc.h>
 #include <maxmod.h>
 #include <string.h>
 
@@ -17,10 +17,53 @@
 // Audio
 #include "soundbank.h"
 #include "soundbank_bin.h"
+ */
+#include <libps.h>
+#include <stdio.h>
+#include <main.h>
 
-void init()
+//--------------------------------------------------------------------------
+// G L O B A L S
+//--------------------------------------------------------------------------
+
+int output_buffer_index;            // buffer index
+GsOT world_ordering_table[2];       // ordering table headers
+GsOT_TAG ordering_table[2][1<<1];   // actual ordering tables
+PACKET gpu_work_area[2][24000];     // GPU packet work area
+u_char prev_mode;					// previous code
+int fnt_id[9];						// font id
+
+
+int main(void)
 {
-    irq_init(NULL);
+    printf("main started: \n");
+
+    //init();
+    Init();
+
+    //main loop
+	while(1)
+    {
+        printf("loop start: \n");
+
+        FntPrint(fnt_id[0], (char*)"WELCOME TO BALATRO!");
+        UpdateScreen();
+
+        /*         
+        VBlankIntrWait();
+        mmFrame();
+		key_poll();
+        update();
+        draw(); */
+    } //end main loop
+    
+    DeInitGame(); //de-innit game
+	return 0;
+}
+
+void Init( void )
+{
+/*     irq_init(NULL);
     irq_add(II_VBLANK, mmVBlank);
     irq_add(II_HBLANK, affine_background_hblank);
 
@@ -76,10 +119,88 @@ void init()
     card_init();
     blind_init();
     joker_init();
-    game_init();
+    game_init(); */
+
+    int count;
+     
+     printf("Starting InitGame() \n");
+    
+  
+     // all reset, the drawing environment and display are initialised
+     ResetGraph(0);
+
+    //This function MUST be called before using other libGS functions!
+     GsInitGraph( SCREEN_WIDTH, SCREEN_HEIGHT,
+                  GsOFSGPU|GsINTER, 0, 0 );
+
+
+     // load in the font pattern
+     FntLoad(960,256);
+     printf("Fonts loaded: \n");
+
+     fnt_id[0] = FntOpen(0,10,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[1] = FntOpen(0,20,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[2] = FntOpen(0,30,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[3] = FntOpen(0,40,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+
+     fnt_id[4] = FntOpen(0,120,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[5] = FntOpen(0,130,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[6] = FntOpen(0,140,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+     fnt_id[7] = FntOpen(0,150,SCREEN_WIDTH, SCREEN_HEIGHT,0,80);
+      
+        // save current video mode
+     prev_mode = GetVideoMode();
+
+     // init graphic mode
+     SetVideoMode( MODE_PAL );
+     printf("Set video mode complete: \n");
+
+             // double buffer definition
+     GsDefDispBuff( 0, 0, 0, 0 );
+     printf("Double buffer setup complete: \n");
+
+     
+     // set display output on t.v 
+     GsDISPENV.screen.x = 10;
+     GsDISPENV.screen.y = 18;
+     GsDISPENV.screen.w = 255;
+     GsDISPENV.screen.h = 255; 
+     
+     // set bg clear color and flag
+     GsDRAWENV.r0 = 0x00;
+     GsDRAWENV.g0 = 0x00;
+     GsDRAWENV.b0 = 0x80;
+     GsDRAWENV.isbg = 1;	  	
+      
+     // set up the ordering table handlers
+     for( count=0; count < 2; count++ )
+        {
+          world_ordering_table[count].length = 1;
+          world_ordering_table[count].org = ordering_table[count];
+        }
+
+     // initialises the ordering table
+     GsClearOt( 0, 0, &world_ordering_table[output_buffer_index]);
+     GsClearOt( 0, 0, &world_ordering_table[output_buffer_index+1]);
+     printf("WOT is setup and complete: \n");
+     printf("Game setup is complete: \n");
 }
 
-void update()
+void DeInitGame( void )
+ {
+
+      // set previous video mode
+     SetVideoMode( prev_mode );
+
+     // current drawing is canvelled and the command queue is flushed
+     ResetGraph(3);	   
+
+     printf("Graphics flushed: \n");
+     printf("Game now de-int: \n");
+ 
+ }// end DeInitGame
+
+/* void update()
 {
     affine_background_update();
     game_update();
@@ -88,20 +209,41 @@ void update()
 void draw()
 {
     sprite_draw();
-}
+} */
 
-int main()
-{
-    init();
+void UpdateScreen( void )
+ {
 
-	while(true)
-    {
-        VBlankIntrWait();
-        mmFrame();
-		key_poll();
-        update();
-        draw();
-    }
+    int count;
 
-	return 0;
-}
+    // get the active buffer
+    output_buffer_index = GsGetActiveBuff();
+
+    // sets drawing command storage address
+    GsSetWorkBase((PACKET*)gpu_work_area[output_buffer_index]);
+
+    // initialises the ordering table
+    GsClearOt(0, 0, &world_ordering_table[output_buffer_index]);
+
+    // rendering done here
+    
+    for( count =0; count <8; count++ )
+         FntFlush(fnt_id[count]);         
+         
+    //GsSortSprite(&sprite, &world_ordering_table[output_buffer_index], 0);           
+        
+    // wait for vertical synchronisation
+    VSync(0);    // 0: blocking until vertical synch occurs
+
+    // swap double buffers, (changes the display buffer and drawing buffer)
+    GsSwapDispBuff(); 
+       
+    // start execution of the drawing command registered in OT
+    GsDrawOt(&world_ordering_table[output_buffer_index]);
+
+ }// end UpdateScreen 
+
+
+
+
+
